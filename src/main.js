4cefee29f7c989cc97f1a41a276e3c5945fa1934
oshanQQ <onoyama1999910@gmail.com>
2021-04-22 22:21:08 +0900

    💬 Set detailed data

--- src/main.js
+++ src/main.js
@@ -4,13 +4,23 @@
 const boxen = require('boxen');
 
 const data = {
-  name: "Onoyama Shodai",
-  handleName: "o-xian",
-  twitter: "https://twitter.com/oshanQQ",
-  github: "https://github.com/oshanQQ",
-  qiita: "https://qiita.com/oshanQQ",
-  blog: "https://o-xian.netlify.app/",
-  speakerdeck: "https://speakerdeck.com/oshanqq"
+  // Basic data
+  name: chalk.white('Onoyama Shodai (o-xian)'),
+  handleName: chalk.hex('#FD9FBA')('oshanQQ'),
+  twitter: chalk.hex('#1DA1F2')('https://twitter.com/oshanQQ'),
+  github: chalk.hex('#888888')('https://github.com/oshanQQ'),
+  qiita: chalk.hex('#55C500')('https://qiita.com/oshanQQ'),
+  blog: chalk.white('https://o-xian.netlify.app/'),
+  speakerdeck: chalk.hex('#009287')('https://speakerdeck.com/oshanqq'),
+
+  // Tags
+  nameTag: chalk.white.bold('       Name: '),
+  handleNameTag: chalk.hex('#FD9FBA').bold(' HandleName: '),
+  twitterTag: chalk.hex('#1DA1F2').bold('    Twitter: '),
+  githubTag: chalk.hex('#888888').bold('     GitHub: '),
+  qiitaTag: chalk.hex('#55C500').bold('      Qiita: '),
+  blogTag: chalk.white.bold('       Blog: '),
+  speakerdeckTag: chalk.hex('#009287').bold('SpeakerDeck: '),
 };
 
 const displayData = data.speakerdeck + "\n" + data.qiita;
